52e1ab4c252d02e5ebbb88ba691ed026fb9fe8f9
yashit sharma <yashitsharma@yashits-MacBook-Air.local>
2024-04-17 10:46:56 +0530

    Questions Added

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,47 +1,47 @@
 const Data = 
 [
-    // {
-    //     Id : 1,
-    //     Question : 'Subscription received in cash during the year amount to Rs. 50,000, the amount received in advance for the next year is Rs. 5,000, the amount 			outstanding for the current year was Rs. 5,000. The amount to be debited to 		the Income and Expenditure Account is',
-    //     Options : ["50,000","60,000","55,000",'None of these'],
-    //     Answer : '50,000'
-    // },
-    // {
-    //     Id : 2,
-    //     Question : 'According to the Partnership Act, the relation of partnership arises from ___________ and not from status.',
-    //     Options : ['Business','Statute', 'Contract','Consideration'],
-    //     Answer : 'Statute'
-    // },
-    // {
-    //   Id : 3,
-    //   Question: "Other income is Rs. 5,00,000 which is 25% of Revenue from operations. Employees benefit Expenses are 30% of the Revenue from operation. Tax rate is 40%. Net profit after tax will be:",
-    //   Options : ["10,25,000", "11,40,000", "10,75,000","12,30,000"],
-    //   Answer : "11,40,000"
-    // },
-    // {
-    //   Id : 4,
-    //   Question : "An annual report is furnished by a company to its:",
-    //   Options : ["Directors", "Auditors", "Shareholders","None"],
-    //   Answer : "Directors"
-    // },
-    // {
-    //   Id : 5,
-    //   Question : "Legend can be repositioned on the chart:",
-    //   Options : ["anywhere", "on the corner only", "on the right side only", "on the bottom of X-axis"],
-    //   Answer : "on the corner only"
-    // },
-    // {
-    //   Id : 6,
-    //   Question : `According to Indian Partnership Act, 1932, when the firm is dissolved, cash received on sale of assets are applied in following order:
-    //   A) Paying to each partner proportionately what is due to him/her on account of capital
-    //   B) In paying the secured debts of the firm to the third parties
-    //   C) In paying each partner proportionately what is due to him/her from the firm for advances as distinguished from capital
-    //   D) The residue, if any shall be divided among the partner's in their profit sharing ratio 
-    //   E) In paying unsecured debt of firm to third parties
-    //   Choose the correct answer from the options given below :`,
-    //   Options : ["(C), (B), (D), (A), (E)", "(B), (E), (C), (A), (D)", "(A), (B), (C), (D), (E)", "(D), (C), (B), (A), (E)"],
-    //   Answer : "(C), (B), (D), (A), (E)"
-    // },
+    {
+        Id : 1,
+        Question : 'Subscription received in cash during the year amount to Rs. 50,000, the amount received in advance for the next year is Rs. 5,000, the amount 			outstanding for the current year was Rs. 5,000. The amount to be debited to 		the Income and Expenditure Account is',
+        Options : ["50,000","60,000","55,000",'None of these'],
+        Answer : '50,000'
+    },
+    {
+        Id : 2,
+        Question : 'According to the Partnership Act, the relation of partnership arises from ___________ and not from status.',
+        Options : ['Business','Statute', 'Contract','Consideration'],
+        Answer : 'Statute'
+    },
+    {
+      Id : 3,
+      Question: "Other income is Rs. 5,00,000 which is 25% of Revenue from operations. Employees benefit Expenses are 30% of the Revenue from operation. Tax rate is 40%. Net profit after tax will be:",
+      Options : ["10,25,000", "11,40,000", "10,75,000","12,30,000"],
+      Answer : "11,40,000"
+    },
+    {
+      Id : 4,
+      Question : "An annual report is furnished by a company to its:",
+      Options : ["Directors", "Auditors", "Shareholders","None"],
+      Answer : "Directors"
+    },
+    {
+      Id : 5,
+      Question : "Legend can be repositioned on the chart:",
+      Options : ["anywhere", "on the corner only", "on the right side only", "on the bottom of X-axis"],
+      Answer : "on the corner only"
+    },
+    {
+      Id : 6,
+      Question : `According to Indian Partnership Act, 1932, when the firm is dissolved, cash received on sale of assets are applied in following order:
+      A) Paying to each partner proportionately what is due to him/her on account of capital
+      B) In paying the secured debts of the firm to the third parties
+      C) In paying each partner proportionately what is due to him/her from the firm for advances as distinguished from capital
+      D) The residue, if any shall be divided among the partner's in their profit sharing ratio 
+      E) In paying unsecured debt of firm to third parties
+      Choose the correct answer from the options given below :`,
+      Options : ["(C), (B), (D), (A), (E)", "(B), (E), (C), (A), (D)", "(A), (B), (C), (D), (E)", "(D), (C), (B), (A), (E)"],
+      Answer : "(C), (B), (D), (A), (E)"
+    },
     {
       Id : 7,
       Question : "Common Size Statements are also known as:",
